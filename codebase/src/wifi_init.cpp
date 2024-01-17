9b0ea1cfbe670a63e6c6d7378fd596ae6c66238d
madman1397 <madman1397@gmail.com>
2024-01-17 11:32:15 -0500

    Removed some dead end code - to be revisited with a different approach at a later date
    Signed-off-by: madman1397 <madman1397@gmail.com>

--- codebase/src/wifi_init.cpp
+++ codebase/src/wifi_init.cpp
@@ -3,6 +3,8 @@
 #include <SPIFFS.h>
 #include <vector>
 
+const String ssid = "";
+const String password = "";
 
 // Scan for local wifi networks - terminal prints out list ordered by signal strength
 std::vector<String> WiFiScan(void) {
@@ -28,48 +30,7 @@ std::vector<String> WiFiScan(void) {
   return SSIDList;
 }
 
-void preAuth(const std::vector<String>& SSIDList) {
-  // Open auth.txt in read mode
-  File file = SPIFFS.open("/auth.txt", "r");
-
-  if (!file) {
-    Serial.println("Failed to open file for reading!");
-    return;
-  }
-  
-  Serial.println("checking for saved credentials...");
-
-  // Read auth file line by line
-  while (file.available()) {
-    String currentSSID = file.readStringUntil('\n');
-    currentSSID.trim(); // Remove leading and trailing whitespace
-
-    auto it = std:: find(SSIDList.begin(), SSIDList.end(), currentSSID);
-    if (it != SSIDList.end()){
-      // Matching SSID found, return next line as the password
-      if (file.available()) {
-        String password = file.readStringUntil('\n');
-        password.trim();
-        Serial.print("Match found for SSID: " + currentSSID);
-        Serial.println(" | pass: " + password);
-        break;
-      }
-    }
-  }
-  file.close();
-}
-
-
-
-
-
-
-
-
-
-
-
-/* void WiFiConnect(void){
+void WiFiConnect(void){
   Serial.println("WifiSetup starting...");
 
   WiFi.begin(ssid, password);
@@ -83,23 +44,8 @@ void preAuth(const std::vector<String>& SSIDList) {
   Serial.print("Connected successfully to network: ");
   Serial.println(ssid);
 }
- */
-/* void saveCredentials(const char *ssid, const char *password) {
-  File file = SPIFFS.open("/auth.txt","w");
-  if (!file) {
-    Serial.println("Failed to open file for writing");
-    return;
-  }
 
-  file.println(ssid);
-  file.println(password);
-
-  file.close();
-  Serial.println("Credentials saved to /preauth.txt");
-} */
 
 void WiFiInit() {
-
   std::vector<String> localSSIDs = WiFiScan();
-  preAuth(localSSIDs);
 }
